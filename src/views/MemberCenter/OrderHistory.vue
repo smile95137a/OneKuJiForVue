d249d822b8a57f3a963823925e8c33745070c7f5
jim <>
2024-08-06 16:57:51 +0800

    會員中心

--- src/views/MemberCenter/OrderHistory.vue
+++ src/views/MemberCenter/OrderHistory.vue
@@ -1,7 +1,101 @@
 <template>
-  <div>o</div>
+  <div class="memberCenter__orderHistory">
+    <MemberCenterCoins />
+    <div class="memberCenter__orderHistoryForm">
+      <div class="memberCenter__orderHistoryForm-title">
+        <p class="memberCenter__text">訂單紀錄</p>
+      </div>
+      <div class="memberCenter__orderHistoryForm-main">
+        <div class="memberCenter__orderHistoryForm-box">
+          <div class="memberCenter__orderHistoryForm-form-inputs m-t-20">
+            <p class="memberCenter__text">起始時間</p>
+            <input
+              type="local-date"
+              class="memberCenter__orderHistoryForm-form-input"
+            />
+          </div>
+        </div>
+        <div class="memberCenter__orderHistoryForm-box">
+          <div class="memberCenter__orderHistoryForm-form-inputs m-t-20">
+            <p class="memberCenter__text memberCenter__text--required">
+              結束時間
+            </p>
+            <input
+              type="local-date"
+              class="memberCenter__orderHistoryForm-form-input"
+            />
+          </div>
+        </div>
+        <div
+          class="memberCenter__orderHistoryForm-box memberCenter__orderHistoryForm-box--btns"
+        >
+          <div class="memberCenter__orderHistoryForm-btns">
+            <div class="memberCenter__orderHistoryForm-btn">
+              <span class="memberCenter__text">查詢</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="memberCenter__table">
+      <table>
+        <thead>
+          <tr>
+            <th class="w-30">日期</th>
+            <th class="w-50">項目</th>
+            <th class="w-20">內容</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+          <tr>
+            <td>2024/07/01 18:22</td>
+            <td>項目名稱</td>
+            <td>內容文字</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import MemberCenterCoins from '@/components/Frontend/memberCenter/MemberCenterCoins.vue';
+</script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
