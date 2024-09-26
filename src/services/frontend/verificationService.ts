fa5dcd5ffb3b0dd519651eee8fafeaebebd3cb3a
jim <>
2024-09-26 23:55:02 +0800

    調整功能

--- src/services/frontend/verificationService.ts
+++ src/services/frontend/verificationService.ts
@@ -10,9 +10,9 @@ export const verifyUser = async (
   token: string
 ): Promise<VerificationResponse> => {
   try {
-    const response = await api.get<VerificationResponse>(basePath, {
-      params: { token },
-    });
+    const response = await api.post<VerificationResponse>(
+      `${basePath}/${token}`
+    );
     return response.data;
   } catch (error) {
     console.error('Error during verification:', error);
