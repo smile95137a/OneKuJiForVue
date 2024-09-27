171d03c7e0e9c7a2d7cb90658c1790d538a4cb57
jim <>
2024-09-27 23:47:31 +0800

    調整版型

--- src/views/CustomizedDraw/CustomizedDraw.vue
+++ src/views/CustomizedDraw/CustomizedDraw.vue
@@ -3,7 +3,7 @@
     <div class="product3__title">
       <div class="product3__text" data-text="客製化抽獎">客製化抽獎</div>
     </div>
-    <NoData />
+    <NoData message="功能尚未開放" />
   </div>
 </template>
 
