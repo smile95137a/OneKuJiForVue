d1c4960712f17fdc4fe583b296c172afd97f7357
jim <>
2024-10-24 15:58:38 +0800

    調整功能

--- src/views/MemberCenter/DrawHistory.vue
+++ src/views/MemberCenter/DrawHistory.vue
@@ -44,13 +44,26 @@
     </div>
     <NoData v-if="orders.length === 0" />
 
-    <div v-else class="memberCenter__table">
-      <DrawFlip
-        v-for="(order, index) in orders"
-        :key="index"
-        :orderData="order"
-        :index="index"
-      />
+    <div v-else class="flex flex-column gap-y-8">
+      <div class="flex gap-x-12" style="border-bottom: 1px solid #000">
+        <div class="w-25">圖片</div>
+        <div class="w-25">名稱</div>
+        <div class="w-25">數量</div>
+        <div class="w-25">日期</div>
+        <div class="w-25">金額</div>
+      </div>
+      <div class="flex gap-x-12" v-for="order in orders" :key="order.id">
+        <div class="w-25"><MImage :src="order.imageUrls[0]" /></div>
+        <div class="w-25">{{ order.productName }}</div>
+        <div class="w-25">{{ order.drawCount }}</div>
+        <div class="w-25">
+          <DateFormatter :date="order.createDate" format="YYYY/MM/DD" />
+        </div>
+        <div class="w-25">
+          <NumberFormatter :number="order.amount ?? 0" />
+          <span v-if="order.payType"> ({{ mapPayType(order.payType) }}) </span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -58,11 +71,12 @@
 <script lang="ts" setup>
 import NoData from '@/components/common/NoData.vue';
 import MemberCenterCoins from '@/components/frontend/memberCenter/MemberCenterCoins.vue';
-import DrawFlip from '@/components/common/DrawFlip.vue';
-import { queryDrawOrder, queryOrder } from '@/services/frontend/orderService';
+import DateFormatter from '@/components/common/DateFormatter.vue';
+import NumberFormatter from '@/components/common/NumberFormatter.vue';
+import MImage from '@/components/frontend/MImage.vue';
+import { queryDrawOrder } from '@/services/frontend/orderService';
 import { useForm } from 'vee-validate';
 import { ref } from 'vue';
-import { useRouter } from 'vue-router';
 
 const orders = ref([]);
 
@@ -84,6 +98,25 @@ const submitForm = handleSubmit(async (values) => {
     console.error('Error fetching order data:', error);
   }
 });
+
+const mapPayType = (payType: number | null | undefined) => {
+  if (payType === null || payType === undefined) {
+    return '未知';
+  }
+
+  switch (~~payType) {
+    case 1:
+      return '金幣';
+    case 2:
+      return '銀幣';
+    case 3:
+      return '紅利';
+    case 4:
+      return '兌換卷';
+    default:
+      return '未知';
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
