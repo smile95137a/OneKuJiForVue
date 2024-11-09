812802e6ab98181ee971282f20527b9fca38a68d
Jimmy <a0970124936@gmail.com>
2024-11-09 17:26:45 +0800

    no message

--- src/components/Backend/ProductDataManagement.vue
+++ src/components/Backend/ProductDataManagement.vue
@@ -774,7 +774,6 @@ const handleProductSubmit = async () => {
     } else if (typeof selectedCategoryId.value === 'number') {
       productForm.categoryId = selectedCategoryId.value;
     }
-debugger
     let response;
     if (editingProduct.value) {
 
