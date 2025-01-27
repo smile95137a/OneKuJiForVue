0ce1a5bd6f0025ef6b1cd19f3ae536ce3d037465
jim <>
2025-01-27 11:47:23 +0800

    調整功能

--- src/views/Maintenance.vue
+++ src/views/Maintenance.vue
@@ -3,7 +3,39 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
 import NoData from '@/components/common/NoData.vue';
+import { useRouter } from 'vue-router';
+import { getDisplayNews } from '@/services/frontend/newsService';
+
+const router = useRouter();
+let intervalId: ReturnType<typeof setInterval> | undefined = undefined;
+
+const processMarqueeData = async () => {
+  try {
+    const { success, data, message } = await getDisplayNews();
+    if (success) {
+      router.push('/home');
+    } else {
+      console.error('連接失敗:', message);
+    }
+  } catch (error) {
+    console.error('連接失敗:', error);
+  }
+};
+
+onMounted(() => {
+  processMarqueeData();
+  intervalId = setInterval(processMarqueeData, 20000);
+});
+
+onUnmounted(() => {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+/* Add your styles here */
+</style>
